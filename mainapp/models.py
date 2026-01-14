1f3d9a4a137916a44ee7ec4479524c01d9626f7b
Saber_Jiten <jitendrakarki988036@gmail.com>
2026-01-14 11:18:59 +0545

    Add: build.sh

--- mainapp/models.py
+++ mainapp/models.py
@@ -14,7 +14,7 @@ class Users(TimeStampedModel):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    contact= models.IntegerField(max_length=10, unique=True)
+    contact= models.IntegerField(unique=True)
     password = models.CharField(max_length=128)
     confirm_password = models.CharField(max_length=128)
     
