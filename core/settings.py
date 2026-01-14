1dd5c4f876df726a127587d92159dfc397798acd
Saber_Jiten <jitendrakarki988036@gmail.com>
2026-01-14 11:15:04 +0545

    Add: build.sh

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-lns+hdkev3pw*oes90x0+$pdzrnc03i4gr-#$n_z1^_n%_k&-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
